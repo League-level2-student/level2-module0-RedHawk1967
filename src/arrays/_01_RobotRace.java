6e43391c6d44b29a3da34a9d0103f8f76f6e96b0
League Student <league@iMac-12.attlocal.net>
2020-01-25 12:55:51 -0800

    done with array cheat sheet onto robot race

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,12 +1,21 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+	public static void main(String[] args) {
+		
+	Robot[] robots = new Robot[5];
 	//2. create an array of 5 robots.
 
 	//3. use a for loop to initialize the robots.
-
+	for (int i = 0; i < robots.length; i++) {
+		robots[i] = new Robot();
+		robots[i].setX(0); robots[i] .setY(600);
+	
+	
+	}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
@@ -16,7 +25,14 @@ public class _01_RobotRace {
 
 	//7. declare that robot the winner and throw it a party!
 
+	
+	
+	
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
-}
+	
+	
+	
+	}
+	}
